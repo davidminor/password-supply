ccee498298208b2a74409304a937d6c903e93aaf
David Minor <daveminor@gmail.com>
2015-02-06 17:46:55 -0800

    number/symbol substitution, basic styling

--- scripts/password-generator.js
+++ scripts/password-generator.js
@@ -2,48 +2,93 @@
 
 var React = require('react/addons'),
     markov = require('markov-words'),
-    stats = require('./stats').stats,
+    stats = require('./stats'),
     generate = function(length, number) {
         return markov.generate(stats, length, number);
-    };
+    },
+    l33t = require('l33t-substitution');
 
 var Password = React.createClass({
   render: function() {
-    return <span>{this.props.text}</span>
+    if (this.props.substitute) {
+      return <code>{l33t(this.props.text, 2)}{' '}</code>
+    } else {
+      return <code>{this.props.text}{' '}</code>
+    }
+
   }
 });
 
 var PasswordList = React.createClass({
+  createState: function(inputLength, inputNumber) {
+    var length = parseInt(inputLength);
+    var lengthValid = !isNaN(length) && length >= 3;
+    var number = parseInt(inputNumber);
+    var numberValid = !isNaN(number) && number > 0;
+    var items = [];
+    if (lengthValid && numberValid) {
+      try {
+        items = generate(length, number);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+    return {items: items, lengthValid: lengthValid, numberValid: numberValid};
+  },
   getInitialState: function() {
-    return {items: generate(this.props.length, this.props.number)};
+    return this.createState(this.props.length, this.props.number);
   },
   componentWillReceiveProps: function(nextProps) {
-    this.setState({items: generate(nextProps.length, nextProps.number)});
+    if (this.props.length !== nextProps.length || 
+        this.props.number !== nextProps.number) {
+      this.setState(this.createState(nextProps.length, nextProps.number));
+    }
+  },
+  refresh: function() {
+    this.setState(this.createState(this.props.length, this.props.number));
   },
   render: function() {
-    return <ul>
-             {this.state.items.map(function(item) {
-               return <li><Password text={item} /></li>
-             })}
-           </ul>
+    var substitute = this.props.substitute;
+    return <div>
+             <ul>
+               {this.state.items.map(function(item) {
+                 return <li className="password">
+                          <Password text={item} substitute={substitute} />
+                        </li>
+               })}
+             </ul>
+           </div>
   }
 });
 
 var PasswordGenerator = React.createClass({
   mixins: [React.addons.LinkedStateMixin],
   getInitialState: function() {
-    return {length: 10, number: 20};
+    return {length: 10, number: 20, substitute: false};
+  },
+  handleRefresh: function() {
+    this.refs.passwordList.refresh();
   },
   render: function() {
     return (
       <div>
-        <label>Password length 
-            <input valueLink={this.linkState('length')} />
-        </label>
-        <label>Number to generate 
-            <input valueLink={this.linkState('number')} />
-        </label>
-        <PasswordList length={this.state.length} number={this.state.number} />
+        <div className="console">
+          <label>Password length{' '} 
+            <input type="text" valueLink={this.linkState('length')} />
+          </label> <label>Number to generate{' '}
+            <input type="text" valueLink={this.linkState('number')} />
+          </label> <label>
+            <input type="checkbox" checkedLink={this.linkState('substitute')} />
+            {' '}Numbers & Symbols
+          </label>
+        </div>
+        <div className="actions">
+          <button className="clearfix" 
+            onClick={this.handleRefresh}>Regenerate</button>
+          <span className="clearfix" />
+        </div>
+        <PasswordList length={this.state.length} number={this.state.number}
+          substitute={this.state.substitute} ref="passwordList" />
       </div>
     );
   }
